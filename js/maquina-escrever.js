3ce794f8dc4512e843edf174b04f5801041f434f
Luciano-Bispo <lucianobispom@gmail.com>
2019-11-27 20:36:57 -0300

    alterações no card da pagina index e feita identação dos arquivos

--- js/maquina-escrever.js
+++ js/maquina-escrever.js
@@ -1,15 +1,12 @@
 function typeWrite(elemento) {
     const textoArray = elemento.innerHTML.split('');
-   elemento.innerHTML = '';
+    elemento.innerHTML = '';
     textoArray.forEach((letra, i) => {
-        setTimeout(function(){
-            elemento.innerHTML+= letra
+        setTimeout(function() {
+            elemento.innerHTML += letra
         }, 75 * i)
     });
 }
 
-const titulo = document.getElementById('titulo_home'); 
+const titulo = document.getElementById('titulo_home');
 typeWrite(titulo);
-
-
-
